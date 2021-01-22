9c5e0a0eca4b6a66d09bcf355da85c3508754bd8
Toni Panacek <toni.panacek@gmail.com>
2021-01-22 10:57:55 +0100

    messing with click event

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,10 @@
 const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
+
+const wordBoxes = document.querySelectorAll('.word-box');
+const quoteBox = document.querySelector('.quote-box');
+wordBoxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    quoteBox.classList.remove('hidden');
+    quoteBox.classList.add('flex');
+  })
+})
