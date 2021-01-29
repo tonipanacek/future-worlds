ce1cbfa200fd7fbfc44aa26ce4061e89b5f03637
Toni Panacek <toni.panacek@gmail.com>
2021-01-29 01:13:43 +0100

    added initial audio clip

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -2,9 +2,23 @@ const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
 
 const wordBoxes = document.querySelectorAll('.word-box');
 const quoteBox = document.querySelector('.quote-box');
+const overlayDiv = document.querySelector('.overlay-div');
+const closeBtn = document.querySelector('.close-btn');
+
 wordBoxes.forEach((box) => {
   box.addEventListener('click', () => {
     quoteBox.classList.remove('hidden');
     quoteBox.classList.add('flex');
+    overlayDiv.classList.add('overlay');
+    quoteBox.querySelector('#empowering').play();
   })
+})
+
+closeBtn.addEventListener('click', () => {
+  quoteBox.classList.remove('flex');
+  quoteBox.classList.add('hidden');
+  overlayDiv.classList.remove('overlay');
+  const audio = quoteBox.querySelector('#empowering')
+  audio.pause();
+  audio.currentTime = 0;
 })
