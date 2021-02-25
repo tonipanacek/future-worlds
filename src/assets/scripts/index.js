9ca458876ac58fcbc839fb61e71e971ad13d2d3c
Toni Panacek <toni.panacek@gmail.com>
2021-02-25 11:00:05 +0100

    form updates

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -4,21 +4,43 @@ const wordBoxes = document.querySelectorAll('.word-box');
 const quoteBox = document.querySelector('.quote-box');
 const overlayDiv = document.querySelector('.overlay-div');
 const closeBtn = document.querySelector('.close-btn');
+const expandableForm = document.querySelector('.collapsible');
 
-wordBoxes.forEach((box) => {
-  box.addEventListener('click', () => {
-    quoteBox.classList.remove('hidden');
-    quoteBox.classList.add('flex');
-    overlayDiv.classList.add('overlay');
-    quoteBox.querySelector('#empowering').play();
-  })
-})
+if (wordBoxes.length !== 0) {
+  wordBoxes.forEach((box) => {
+    box.addEventListener('click', () => {
+      quoteBox.classList.remove('hidden');
+      quoteBox.classList.add('flex');
+      overlayDiv.classList.add('overlay');
+      quoteBox.querySelector('#empowering').play();
+    })
+  });
 
-closeBtn.addEventListener('click', () => {
-  quoteBox.classList.remove('flex');
-  quoteBox.classList.add('hidden');
-  overlayDiv.classList.remove('overlay');
-  const audio = quoteBox.querySelector('#empowering')
-  audio.pause();
-  audio.currentTime = 0;
-})
+  closeBtn.addEventListener('click', () => {
+    quoteBox.classList.remove('flex');
+    quoteBox.classList.add('hidden');
+    overlayDiv.classList.remove('overlay');
+    const audio = quoteBox.querySelector('#empowering')
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
+
+
+// if (expandableForm) {
+//   const main = document.querySelector('.your-say-grid');
+//   expandableForm.addEventListener('click', () => {
+//     expandableForm.classList.toggle('active');
+//     const content = document.querySelector('.content');
+//     if (expandableForm.classList.contains('active')) {
+//       content.style.width = '350px';
+//       main.style.marginRight = '350px';
+//     } else {
+//       content.style.width = '0';
+//       main.style.marginRight = '0';
+//     }
+//   })
+// }
+
+
+
---
+++ src/have-your-say.md
@@ -0,0 +1,8 @@
+---
+layout: your-say
+title: Your Future Worlds - Have Your Say
+page-title: Have Your Say
+bgColor: red
+question1: What do you imagine Hackney to be like in 50 years time?
+question2: What would you like to change for the future?
+---
