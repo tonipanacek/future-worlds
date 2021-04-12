7296d7ced8a3753c8e97a59fc2af945eb2b342ed
Toni Panacek <toni.panacek@gmail.com>
2021-04-12 09:04:47 +0200

    adjusting for mobile homepage

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,9 +1,8 @@
 // const foo = (() => Promise.resolve().then(() => console.log('Hello world')))()
 
 const wordBoxes = document.querySelectorAll('.word-box');
-// const quoteBox = document.querySelector('.quote-box');
 const overlayDiv = document.querySelector('.overlay-div');
-const closeBtns = document.querySelectorAll('.close-btn');
+// const closeBtns = document.querySelectorAll('.close-btn');
 const expandableForm = document.querySelector('.collapsible');
 const body = document.body;
 
@@ -12,44 +11,22 @@ if (wordBoxes.length !== 0) {
     const id = box.id;
     const quote = document.querySelector(`#${id}-quote`);
     const audio = quote.querySelector('audio');
+    const closeBtn = quote.querySelector('.close-btn');
     box.addEventListener('click', () => {
       quote.classList.remove('hidden');
       quote.classList.add('flex');
       overlayDiv.classList.add('overlay');
       body.classList.add('noscroll');
-      setTimeout(() => audio.play(), 2000)
-    })
-  });
+      setTimeout(() => audio.play(), 1000)
+    });
 
-  closeBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-      const quote = button.parentElement;
-      const audio = quote.querySelector('audio');
+    closeBtn.addEventListener('click', () => {
       quote.classList.remove('flex');
       quote.classList.add('hidden');
       overlayDiv.classList.remove('overlay');
       body.classList.remove('noscroll');
       audio.pause();
       audio.currentTime = 0;
-    })
+    });
   });
 }
-
-
-// if (expandableForm) {
-//   const main = document.querySelector('.your-say-grid');
-//   expandableForm.addEventListener('click', () => {
-//     expandableForm.classList.toggle('active');
-//     const content = document.querySelector('.content');
-//     if (expandableForm.classList.contains('active')) {
-//       content.style.width = '350px';
-//       main.style.marginRight = '350px';
-//     } else {
-//       content.style.width = '0';
-//       main.style.marginRight = '0';
-//     }
-//   })
-// }
-
-
-
